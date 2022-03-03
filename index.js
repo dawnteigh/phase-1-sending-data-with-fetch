6e0b9d367412413c5be09e679ad1c0b8f1c4ee18
Donte Ladatto <donte279@gmail.com>
2022-03-03 11:18:43 -0600

    commit tree 86850c562b228fda943834dc2d2a74bbd78b70bb

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const body = document.querySelector('body');
+function addNewContact(newID){
+  body.innerHTML = newID;
+  document.body.append(message)
+}
+function submitData(name, email) {
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+          name: name,
+          email: email,
+      }),
+    };
+      return fetch("http://localhost:3000/users", configurationObject)
+      .then(res => res.json())
+      .then(body => addNewContact(body.id))
+      .catch(function (error) {
+        alert("Unauthorized Access");
+        body.append(error)
+      })
+  }
+  submitData("steve", "steve@website.com")
